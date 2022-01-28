776a247e779cb8870e7e65b7cacd7964eb7c1785
Andrew Robles <andrewrobles@verizon.net>
2022-01-28 07:20:47 -0800

    Convert app component from class to function

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Globe from 'worldwind-react-globe'
 import { Container } from 'reactstrap'
 
 import './App.css'
 
-export default class App extends Component {
-  
-  render() {
-    
-    const layers = [
-      {layer: 'blue-marble', options: {category: 'base', enabled: true}},
-      {layer: 'blue-marble-landsat', options: {category: 'base', enabled: false}},
-      {layer: 'bing-aerial', options: {category: 'base', enabled: false}},
-      {layer: 'bing-aerial-labels', options: {category: 'base', enabled: false}},
-      {layer: 'eox-sentinal2', options: {category: 'base', enabled: false}},
-      {layer: 'eox-sentinal2-labels', options: {category: 'base', enabled: true}},
-      {layer: 'eox-openstreetmap', options: {category: 'overlay', enabled: false, opacity: 0.8}},
-      {layer: 'bing-roads', options: {category: 'overlay', enabled: false, opacity: 0.8}},
-      {layer: 'renderables', options: {category: 'data', enabled: true, displayName: 'Markers'}},
-      {layer: 'compass', options: {category: 'setting', enabled: false}},
-      {layer: 'coordinates', options: {category: 'setting', enabled: true}},
-      {layer: 'view-controls', options: {category: 'setting', enabled: true}},
-      {layer: 'stars', options: {category: 'setting', enabled: false}},
-      {layer: 'atmosphere-day-night', options: {category: 'setting', enabled: false}}
-    ]
-    
-    return (
-      <div>
-        <Container fluid className='p-0'>
-          <div className='globe'>
-              <Globe 
-                layers={layers}/>
-          </div>
-        </Container>
-      </div>
-    )
-  }
+export default function App() {
+  const layers = [
+    {layer: 'blue-marble', options: {category: 'base', enabled: true}},
+    {layer: 'blue-marble-landsat', options: {category: 'base', enabled: false}},
+    {layer: 'bing-aerial', options: {category: 'base', enabled: false}},
+    {layer: 'bing-aerial-labels', options: {category: 'base', enabled: false}},
+    {layer: 'eox-sentinal2', options: {category: 'base', enabled: false}},
+    {layer: 'eox-sentinal2-labels', options: {category: 'base', enabled: true}},
+    {layer: 'eox-openstreetmap', options: {category: 'overlay', enabled: false, opacity: 0.8}},
+    {layer: 'bing-roads', options: {category: 'overlay', enabled: false, opacity: 0.8}},
+    {layer: 'renderables', options: {category: 'data', enabled: true, displayName: 'Markers'}},
+    {layer: 'compass', options: {category: 'setting', enabled: false}},
+    {layer: 'coordinates', options: {category: 'setting', enabled: true}},
+    {layer: 'view-controls', options: {category: 'setting', enabled: true}},
+    {layer: 'stars', options: {category: 'setting', enabled: false}},
+    {layer: 'atmosphere-day-night', options: {category: 'setting', enabled: false}}
+  ]
+
+  return (
+    <div>
+      <Container fluid className='p-0'>
+        <div className='globe'>
+            <Globe 
+              layers={layers}/>
+        </div>
+      </Container>
+    </div>
+  )
 }
